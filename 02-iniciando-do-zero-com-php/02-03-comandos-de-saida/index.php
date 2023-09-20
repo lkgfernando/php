8200822dedfe7d50cb7456a99244da36d3989f81
lkgfernando <lkgfernando@gmail.com>
2023-09-20 11:08:29 -0300

    #fsphp

--- 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
+++ 02-iniciando-do-zero-com-php/02-03-comandos-de-saida/index.php
@@ -7,32 +7,80 @@ fullStackPHPClassName("02.03 - Comandos de saída");
  */
 fullStackPHPClassSession("echo", __LINE__);
 
+echo "<p>Olá Mundo", " ", "<span class='tag'> #BoraProgramar! </span></p>";
+
+$hello = "Olá Mundo";
+$code = "<span class='tag'> #BoraProgramar! </span>";
+
+echo "<p>$hello</p>";
+echo "<p>$code</p>";
+
+$day = "dia";
+echo "<p>Faltam 1 $day para o evendo!</p>";
+echo "<p>Faltam 2 {$day}s para o evento!</p>";
+
+echo "<h3>{$hello}</h3>";
+echo "<h4>{$hello} {$code} </h4>";
+
+echo "<h3>".$hello."".$code."</h3>";
+
 
 /**
  * [ print ] https://php.net/manual/pt_BR/function.print.php
  */
 fullStackPHPClassSession("print", __LINE__);
 
+print $hello;
+print $code;
+
+print "<h3>{$hello}{$code}</h3>";
 
 /**
  * [ print_r ] https://php.net/manual/pt_BR/function.print-r.php
  */
 fullStackPHPClassSession("print_r", __LINE__);
 
+$array = [
+    "company" => "UpInside",
+    "course" => "FSPHP",
+    "class" => "Comandos de saída"
+];
+
+print_r($array);
+
+echo "<pre>", print_r($array, true), "</pre>";
 
 /**
  * [ printf ] https://php.net/manual/pt_BR/function.printf.php
  */
 fullStackPHPClassSession("printf", __LINE__);
 
+$article = "<article><h1>%s</h1><p>%s</p></article>";
+$title = "{$hello} {$code}";
+$subtitle = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+
+printf($article,$title, $subtitle);
+echo sprintf($article, $title, $subtitle);
 
 /**
  * [ vprintf ] https://php.net/manual/pt_BR/function.vprintf.php
  */
 fullStackPHPClassSession("vprintf", __LINE__);
 
+$company = "<article><h1>Escola %s</h1> <p>Curso <b>%s</b>, aula <b>%s</b> </p > </article>";
+vprintf($company, $array);
+vsprintf($company, $array);
+
 
 /**
  * [ var_dump ] https://php.net/manual/pt_BR/function.var-dump.php
  */
 fullStackPHPClassSession("var_dump", __LINE__);
+
+var_dump($array);
+
+var_dump(
+    $array,
+    $hello,
+    $company
+);
--- fullstackphp/template.php
+++ fullstackphp/template.php
@@ -3,6 +3,6 @@ require __DIR__ . '/../../fullstackphp/fsphp.php';
 fullStackPHPClassName("CLASS_TITLE");
 
 /*
- * 
+ *
  */
 fullStackPHPClassSession("", __LINE__);
